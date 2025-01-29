5de04eec0e254342001931b824d71af30876fb56
matsudamper <let@matsudamper.net>
2025-01-30 07:36:57 +0900

    終了を追加

--- src/jvmMain/kotlin/net/matsudamper/gptclient/Main.kt
+++ src/jvmMain/kotlin/net/matsudamper/gptclient/Main.kt
@@ -3,10 +3,11 @@ package net.matsudamper.gptclient
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import net.matsudamper.gptclient.datastore.SettingDataStore
+import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
     application {
-        Window(onCloseRequest = {}) {
+        Window(onCloseRequest = { exitProcess(0) }) {
             App(
                 settingDataStore = SettingDataStore(
                     filename = "setting"
