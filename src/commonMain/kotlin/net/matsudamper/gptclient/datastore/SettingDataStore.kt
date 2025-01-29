aeed4300a5edbdc323103198d8518cfc6c83662b
matsudamper <let@matsudamper.net>
2025-01-30 05:14:14 +0900

    format

--- src/commonMain/kotlin/net/matsudamper/gptclient/datastore/SettingDataStore.kt
+++ src/commonMain/kotlin/net/matsudamper/gptclient/datastore/SettingDataStore.kt
@@ -6,7 +6,6 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.last
 import okio.Path.Companion.toPath
 
 class SettingDataStore(private val filename: String) {
