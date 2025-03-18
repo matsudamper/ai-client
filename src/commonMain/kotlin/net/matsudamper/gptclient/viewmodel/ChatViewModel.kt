36455e7018329397ec280396b2978cc86977e98d
matsudamper <let@matsudamper.net>
2025-03-19 02:39:23 +0900

    UiStateに詰めていなかったので修正

--- src/commonMain/kotlin/net/matsudamper/gptclient/viewmodel/ChatViewModel.kt
+++ src/commonMain/kotlin/net/matsudamper/gptclient/viewmodel/ChatViewModel.kt
@@ -152,6 +152,7 @@ class ChatViewModel(
                                 }
                             },
                         ),
+                        errorDialogMessage = viewModelState.errorDialogMessage,
                     )
                 }
             }
