c5c31bcd7678223d94f6b25f837530cf71caf3cb
matsudamper <matsudamper971227@gmail.com>
2026-09-16 07:55:20 +0900

    再実行終了時にWorker状態をクリアする

--- app-android/src/main/kotlin/net/matsudamper/gptclient/worker/ChatRequestWorker.kt
+++ app-android/src/main/kotlin/net/matsudamper/gptclient/worker/ChatRequestWorker.kt
@@ -36,11 +36,13 @@ class ChatRequestWorker(
     private val localModelAiClientFactory: LocalModelAiClientFactory = GlobalContext.get().get()
 
     override suspend fun doWork(): Result {
+        val chatRoomId = ChatRoomId(inputData.getLong(KEY_CHAT_ROOM_ID, 0))
         if (runAttemptCount > 0) {
+            appDatabase.chatRoomDao().update(id = chatRoomId) {
+                it.copy(workerId = null)
+            }
             return Result.failure()
         }
-
-        val chatRoomId = ChatRoomId(inputData.getLong(KEY_CHAT_ROOM_ID, 0))
 
         val chatRoom = appDatabase.chatRoomDao()
 
